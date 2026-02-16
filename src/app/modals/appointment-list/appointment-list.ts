913d5bce50f126b03f65be9c035ff92f3a60b89c
Deimer Garcia <deimergarcia.dev@gmail.com>
2026-02-16 11:13:59 -0500

    feat: se aplicó pipe de hora al modal appointment-list

--- src/app/modals/appointment-list/appointment-list.ts
+++ src/app/modals/appointment-list/appointment-list.ts
@@ -1,15 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf, *ngFor
+import { CommonModule, formatDate } from '@angular/common'; // Importa CommonModule para *ngIf, *ngFor
 import { AppointmentModel } from '../../models/appointment'; // Asegúrate de la ruta correcta
 import { EmployeeModel } from '../../models/employee'; // Asegúrate de la ruta correcta
 import { AppointmentListModel } from '../../models/appointmentList';
 import { FormatDatePipe } from '../../pipes/format-date-pipe';
+import { FormatTimePipe } from '../../pipes/format-time-pipe';
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [MatDialogModule, MatButtonModule, CommonModule, FormatDatePipe],
+  imports: [MatDialogModule, MatButtonModule, CommonModule, FormatDatePipe, FormatTimePipe],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.css',
 })
